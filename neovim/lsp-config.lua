50c45734bb3cd9457629e4cd9718836f293c6b15
= <=>
2023-06-25 19:23:15 +0200

    fix eslint

--- neovim/lsp-config.lua
+++ neovim/lsp-config.lua
@@ -61,7 +61,7 @@ local null_ls = require('null-ls')
 local null_ls_languages = { 'javascript', 'typescript', 'svelte', 'html', 'css', 'json' }
 null_ls.setup({
   sources = (function()
-    local ls = { null_ls.builtins.code_actions.eslint_d.with({ filetypes = null_ls_languages }) }
+    local ls = { null_ls.builtins.diagnostics.eslint_d.with({ filetypes = null_ls_languages }) }
 
     if vim.fn.findfile("yarn.lock") ~= "yarn.lock" then
       table.insert(ls, null_ls.builtins.formatting.prettierd.with({ filetypes = null_ls_languages }))

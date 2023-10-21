23c2cd820b608605ca25b3f34ead1f29680ae3c3
= <=>
2023-10-21 11:08:05 +0200

    fix

--- script/secrets.sh
+++ script/secrets.sh
@@ -1,3 +1,3 @@
-[ -d ~/blixter ] && rm -rf /mnt/d/secrets/blixter/.env && cp -r ~/blixter/.env /mnt/d/secrets/blixter/.env
-[ -d ~/portfolio ] && rm -rf /mnt/d/secrets/portfolio/.env && cp ~/portfolio/.env /mnt/d/secrets/portfolio/.env
-[ -d ~/argocd ] && rm -rf /mnt/d/secrets/argocd/secrets && cp -r ~/argocd/secrets /mnt/d/secrets/argocd/secrets 
+[ -d ~/blixter ] && rm -rf /mnt/d/secrets/blixter/.env && mkdir -p /mnt/d/secrets/blixter && cp ~/blixter/.env /mnt/d/secrets/blixter/.env
+[ -d ~/portfolio ] && rm -rf /mnt/d/secrets/portfolio/.env && mkdir -p /mnt/d/secrets/portfolio && cp ~/portfolio/.env /mnt/d/secrets/portfolio/.env
+[ -d ~/argocd ] && rm -rf /mnt/d/secrets/argocd/secrets && mkdir -p /mnt/d/secrets/argocd && cp -r ~/argocd/secrets /mnt/d/secrets/argocd/secrets 

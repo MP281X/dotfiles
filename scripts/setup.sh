b2891d533d4673a7805cb62f99734eba8932dd7e
MP281X <paludgnachmatteo.dev@gmail.com>
2024-06-18 13:53:27 +0200

    fix setup script

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,35 +1,46 @@
 #!/bin/bash
 
 clear
+
 log() {
 	echo -e ""
 	echo -e "\033[1;33m$1\033[0m"
 	echo -e ""
 }
 
+brewInstall() {
+	export HOMEBREW_NO_ENV_HINTS=1
+	brew install $1 -q 1>/dev/null
+}
+
+code() { 
+	code-server --force --install-extension "$1" >/dev/null 
+}
+
 
 log "packages"
+
 sudo apt-get update > /dev/null && sudo apt-get upgrade -y > /dev/null
 sudo apt-get install -y git curl wget xz-utils unzip  > /dev/null
 sudo apt-get install -y gcc g++  > /dev/null # neovim
 #----------------------------------------------------------------------------------------------------------------
 
 log "brew"
+
 NONINTERACTIVE=1 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
 #----------------------------------------------------------------------------------------------------------------
 
-log "shell"
-brew install eza -q
-brew install starship -q
-brew install neovim -q
-brew install code-server -q
+log "tools"
+
+brewInstall "eza"
+brewInstall "starship"
+brewInstall "neovim"
 #----------------------------------------------------------------------------------------------------------------
 
-log "code-server extensions"
-code() { 
-	code-server --force --install-extension "$1" >/dev/null 
-}
+log "code-server"
+
+brewInstall "code-server"
 
 code "usernamehw.errorlens"
 code "dbaeumer.vscode-eslint"
@@ -45,36 +56,37 @@ code "pomdtr.excalidraw-editor"
 #----------------------------------------------------------------------------------------------------------------
 
 log "kubernetes"
-brew install kubernetes-cli -q
-brew install k9s -q
-brew install kubeseal -q
+
+brewInstall "kubernetes-cli"
+brewInstall "k9s"
+brewInstall "kubeseal"
+
 mkdir -p ~/.kube && cp /mnt/d/secrets/config ~/.kube/config
 #----------------------------------------------------------------------------------------------------------------
 
-# log "nodejs"
-# curl -fsSL https://get.pnpm.io/install.sh | sh -
-# export PNPM_HOME="/home/$USER/.local/share/pnpm"
-# case ":$PATH:" in *":$PNPM_HOME:"*) ;; *) export PATH="$PNPM_HOME:$PATH" ;; esac
-# pnpm env use --global latest
-#----------------------------------------------------------------------------------------------------------------
+log "node / bun"
 
-log "bun"
+brewInstall "node"
+brewInstall "pnpm"
+
 curl -fsSL https://bun.sh/install | bash
 #----------------------------------------------------------------------------------------------------------------
 
 log "docker"
-brew install docker -q
-# curl -fsSL https://get.docker.com | sh
-# sudo usermod -aG docker $USER
+
+curl -fsSL https://get.docker.com | sh
+sudo usermod -aG docker $USER
 #----------------------------------------------------------------------------------------------------------------
 
 log "ssh"
+
 mkdir -p ~/.ssh && cp /mnt/d/secrets/.ssh/id_rsa ~/.ssh/id_rsa && chmod 0400 ~/.ssh/id_rsa
 #----------------------------------------------------------------------------------------------------------------
 
 log "git"
-brew install gh -q
-brew install gitui -q
+
+brewInstall "gh"
+brewInstall "gitui"
 
 gh auth login --git-protocol https --hostname github.com --web --scopes read:packages
 git config --global user.name $(gh api user | jq -r '.name')

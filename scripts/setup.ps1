5a4e49cddf2d84c73041553f7c332d289112aaea
MP281X <paludgnachmatteo.dev@gmail.com>
2024-08-17 23:14:48 +0200

    fix wsl install script

--- scripts/setup.ps1
+++ scripts/setup.ps1
@@ -5,5 +5,5 @@ debian install --root;
 debian run "useradd -m -s /bin/bash $Env:UserName";
 debian run "passwd -d $Env:UserName && usermod -aG sudo $Env:UserName";
 debian config --default-user $Env:UserName;
-debian run "sudo apt-get install curl -y > /dev/null";
+debian run "sudo apt-get update > /dev/null && sudo apt-get install curl -y > /dev/null";
 debian run "bash <(curl -s -L https://raw.githubusercontent.com/MP281X/dotfiles/main/scripts/setup.sh)";

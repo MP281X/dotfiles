1838979e46beca160479e9dd31c2c47a002427b7
MP281X <paludgnachmatteo.dev@gmail.com>
2025-09-29 11:00:55 +0200

    dev: visualize folder names for .ts and .tsx files

--- nvim/lua/plugins/ui.lua
+++ nvim/lua/plugins/ui.lua
@@ -40,7 +40,24 @@ return {
         sections = {
           lualine_a = { "mode" },
           lualine_b = { { "branch", icon = "󰊢" } },
-          lualine_c = { { "buffers", symbols = { modified = " 󱇨", alternate_file = "" } } },
+          lualine_c = {
+            {
+              "buffers",
+              symbols = { modified = " 󱇨", alternate_file = "" },
+              buffers_color = {
+                active = { fg = "#c0caf5", bg = "#1a1b26" },
+                inactive = { fg = "#565f89", bg = "#1a1b26" },
+              },
+              fmt = function(name, context)
+                if name == "index.ts" or name == "index.tsx" then
+                  local full_path = context.bufpath or vim.api.nvim_buf_get_name(context.bufnr)
+                  local parent = vim.fn.fnamemodify(full_path, ":h:t")
+                  return "./" .. parent
+                end
+                return name
+              end
+            }
+          },
           lualine_x = { "diagnostics" },
           lualine_y = { "filetype" },
           lualine_z = { "location" },
